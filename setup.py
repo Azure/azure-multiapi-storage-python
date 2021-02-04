4e4eb8b4be47076a987632bdb0112ef6666bf576
Zunli Hu <zuh@microsoft.com>
2021-02-04 14:24:59 +0800

    refine dependency

--- setup.py
+++ setup.py
@@ -60,10 +60,11 @@ setup(
     packages=find_packages(exclude=["azure"]),
     install_requires=[
         'azure-common',
-        'cryptography',
         'python-dateutil',
         'requests',
-	    'azure-core',
+        "azure-core<2.0.0,>=1.10.0",
+        "msrest>=0.6.18",
+        "cryptography>=2.1.4"
     ],
     extras_require={
         ':python_version=="2.7"': ['futures'],

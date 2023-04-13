ce474738751274b6ef09cb8f61e70c2b56e9abab
Yishi Wang <yishiwang@microsoft.com>
2023-04-13 13:25:27 +0800

    [1.1.0] Add file share v2022_11_02 (#59)
    * add file share v2022_11_02
    
    * remove file share v2021_06_08
    
    * upgrade version

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ except ImportError:
 
 setup(
     name='azure-multiapi-storage',
-    version='1.0.0',
+    version='1.1.0',
     description='Microsoft Azure Storage Client Library for Python with multi API version support.',
     long_description=open('README.rst', 'r').read(),
     license='MIT',
